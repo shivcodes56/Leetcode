b5ab95083ed2f215d74bdadf66b5b34c09e7e9eb
shivcodes56 <shivgupta0905@gmail.com>
2025-09-13 04:04:43 +0530

    Time: 0 ms (100%), Space: 11.5 MB (39.38%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -4,13 +4,13 @@ public:
        int n=digits.size();
        for(int i=n-1;i>=0;i--){
         if(digits[i]==9){
-            digits[i]=0;
+            digits[i]=0;   // ye sirf tab hoga jab last mein 9 ayega 
         }else{
-            digits[i]++;   // agar vo 9 nhi hai like the last number so usko ek se bada do simple 
+            digits[i]++;
             return digits;
         }
        }
-        vector<int>newdigits(n+1,0);   // so hamne ek nayi vector list bana di hai jisme n+1 elements honge sare 0 honge bas phela element 1 hoga and ye bhi tab hoga jab sare elements 9 ho jayega to carry mein 1 ajayega
+       vector<int> newdigits(n+1,0);
         newdigits[0]=1;
         return newdigits;
     }
