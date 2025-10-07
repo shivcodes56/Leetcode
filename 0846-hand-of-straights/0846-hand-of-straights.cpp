8054ccc05f527966ca810aa3e8fa08bd08d9385e
shivcodes56 <shivgupta0905@gmail.com>
2025-10-08 02:46:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        sort(hand.begin(),hand.end());
+        sort(hand.begin(),hand.end());  // sort
         unordered_map<int,int>mp;
         for(int &h:hand){
-            mp[h]+=1;
+// increase count in map or build the map
+            mp[h]=mp[h]+1;
         }
-        for(int &card:hand){
-            if(mp[card]==0)
-                continue;
-           int need=mp[card];
-           for(int i=0;i<groupSize;i++){
-            if(mp[card+i]<need){
-                return false;
-            }else{
-                mp[card+i]=mp[card+i]-need;
-            }
-           }
-            
-
+       // now we have to build the groups 
+       for(int &card:hand){
+        if(mp[card]==0)
+        continue;
+      int need= mp[card];
+       for(int i=0;i<groupSize;i++){
+        if(mp[card+i]<need){  //check if you have enough cards to make the group 
+            return false;
+        }else{
+            mp[card+i]=mp[card+i]-need;  // if you have enough then substract
         }
         return true;
+       }
+      
     }
 };
