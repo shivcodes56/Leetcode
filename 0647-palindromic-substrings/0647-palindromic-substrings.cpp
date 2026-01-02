814f9c6c137a712e79c36ee571f5b217fd17c23f
shivcodes56 <shivgupta0905@gmail.com>
2026-01-03 02:49:12 +0530

    Time: 30 ms (30.64%), Space: 12.8 MB (42.46%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int n;
+int t[1001][1001];
+bool solve(string &s,int i,int j){
+    if(i>=j){
+        return true;
+    }
+    if(t[i][j]!=-1){
+        return t[i][j];
+    }
+    if(s[i]==s[j]){
+        return t[i][j]= solve(s,i+1,j-1);
+    }
+    return false;
+}
+    int countSubstrings(string s) {
+        n=s.length();
+        memset(t,-1,sizeof(t));
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j)==true){
+                  count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+};
