abf3cf687e608e7e0ea6dbfda5aa905086a53c3d
shivcodes56 <shivgupta0905@gmail.com>
2026-01-07 03:10:35 +0530

    Time: 3 ms (70.73%), Space: 22.2 MB (50.27%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool ispalindrome(string &s,int i,int j){
+    while(i<=j){
+        if(s[i]!=s[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+    bool validPalindrome(string s) {
+        int n=s.length();
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return ispalindrome(s,i+1,j)||ispalindrome(s,i,j-1);
+            }
+            i++;
+            j--;
+        }
+      return true;
+    }
+};
