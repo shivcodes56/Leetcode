b1a85439c09aa0fcbb82fa93978c5e21adf591be
Shivam Gupta <shivgupta0905@gmail.com>
2026-02-10 01:14:50 +0530

    Time: 0 ms (100%), Space: 8.5 MB (66.08%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -6,12 +6,12 @@ public:
         int i=0;
         int j=0;
         while(i<m && j<n){
-            if(s[i]==t[j]){
-                i++;
-                j++;
-            }else{
-                j++;
-            }
+          if(s[i]==t[j]){
+            i++;
+            j++;
+          }else{
+            j++;
+          }
         }
         return i==m;
     }
