4b9f985d54c6a5ceda5f18216a1cbe96d5b37082
Shivam Gupta <shivgupta0905@gmail.com>
2026-03-04 03:17:10 +0530

    Time: 42 ms (5.09%), Space: 55.8 MB (5.23%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public: 
+void backtrack(int idx,int sum,vector<int>&temp,int target,vector<vector<int>>&result,vector<int>candidates){
+    if(sum==target){
+        result.push_back(temp);
+        return;
+    }
+    if(sum>target || idx>=candidates.size()){
+        return;
+    }
+    for(int i=idx;i<candidates.size();i++){
+        //handles the duplication part over here 
+        if(i>idx && candidates[i]==candidates[i-1]){
+            continue;
+        }
+        temp.push_back(candidates[i]);
+        backtrack(i+1,sum+candidates[i],temp,target,result,candidates);
+        temp.pop_back();
+
+    }
+}
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int>temp;
+        vector<vector<int>>result;
+        sort(candidates.begin(),candidates.end());
+        backtrack(0,0,temp,target,result,candidates);
+        return result;
+    }
+};
