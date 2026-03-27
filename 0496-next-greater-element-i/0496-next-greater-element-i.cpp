78ff8ff661ec5dd23429698819d399e994a5a49d
Shivam Gupta <shivgupta0905@gmail.com>
2026-03-28 02:20:35 +0530

    Time: 4 ms (23.58%), Space: 12.7 MB (58.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -5,16 +5,15 @@ public:
         unordered_map<int,int>mp;
         int n=nums2.size();
         for(int i=0;i<n;i++){
-            while(!st.empty() &&nums2[i]>st.top()){  
-                 mp[st.top()]=nums2[i];
-                 st.pop();
+            while(!st.empty() && nums2[i]>st.top()){
+                mp[st.top()]=nums2[i];
+                st.pop();
             }
-               st.push(nums2[i]);
+            st.push(nums2[i]);
         }
-        //for remaining elements 
         while(!st.empty()){
-           mp[st.top()]=-1;
-           st.pop();
+            mp[st.top()]=-1;
+            st.pop();
         }
         vector<int>result;
         for(int i=0;i<nums1.size();i++){
