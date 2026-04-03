c3327b4ee7c0ea259feeaf7f823f495e8b620d9e
Shivam Gupta <shivgupta0905@gmail.com>
2026-04-04 03:11:35 +0530

    Time: 0 ms (100%), Space: 10 MB (25.64%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -13,9 +13,9 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode*slow=head;
         ListNode*fast=head;
-        while(fast!=NULL && fast->next!=NULL){
+        while(fast!=nullptr && fast->next!=nullptr){
             slow=slow->next;
-            fast=fast->next->next;   // this approach is the slow and the fast ptr technique
+            fast=fast->next->next;
         }
         return slow;
     }
