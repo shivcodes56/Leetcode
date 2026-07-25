e33e904a872e8746aa8f8302434cc466dbd8b717
Shivam Gupta <shivgupta0905@gmail.com>
2026-07-26 01:41:24 +0530

    Time: 1 ms (11.98%), Space: 15.1 MB (41.33%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -21,19 +21,19 @@ public:
         while(!que.empty()){
             int size=que.size();
             for(int i=0;i<size;i++){
-              TreeNode*node=que.front();
-              if(node->left!=NULL){
-                que.push(node->left);
-              }
-              if(node->right!=NULL){
-                que.push(node->right);
-              }
-              que.pop();
-               if(i==size-1){
-                result.push_back(node->val);
-               }
+                TreeNode*node=que.front();
+                  que.pop();
+                   if(node->left!=NULL){
+            que.push(node->left);
+           }
+            if(node->right!=NULL){
+            que.push(node->right);
+           }
+             if(i==size-1){
+            result.push_back(node->val);
+           }
             }
         }
-           return result;
+        return result;
     }
 };
