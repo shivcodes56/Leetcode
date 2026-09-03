1679dcc66b5c6b063d5c5217614ccf1f926aedc1
Shivam Gupta <shivgupta0905@gmail.com>
2026-09-03 14:50:37 +0530

    Time: 0 ms (100%), Space: 8.4 MB (92.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
-        long long rev=0;  // long long isliye ki overflow na hojaye
-        int temp=x;
-        while(temp>0){
-            // this is how you reverse an integer mathematically 
-            int digit=temp %10;
+        int original=x;
+        long long rev=0;
+        while(x>0){
+            int digit=x%10;
             rev=rev*10+digit;
-            temp=temp/10;
+            x=x/10;
         }
-        if(x==rev){
+        if(rev==original){
             return true;
         }
         return false;
