673b9a537bba0e8a34747bf7ca10a2374d413d93
Shivam Gupta <shivgupta0905@gmail.com>
2026-09-25 00:44:29 +0530

    Time: 3 ms (32.32%), Space: 11.8 MB (41.44%) - LeetHub

---
+++ 2937-make-three-strings-equal/2937-make-three-strings-equal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMinimumOperations(string s1, string s2, string s3) {
+        int i=0;
+        while(i<s1.length() && i<s2.length() && i<s3.length() &&
+        s1[i]==s2[i] && s2[i]==s3[i]){
+            i++;
+        }
+        if(i==0){
+            return -1;
+        }
+       return (s1.length()-i + s2.length()-i + s3.length()-i);
+    }
+};
